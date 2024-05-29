c3a6e1406553df164037e48ba71556cc74447aa7
rfross <48492222+rfross@users.noreply.github.com>
2024-05-29 15:42:20 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,3 +16,22 @@ def run_discord_bot():
   @client.event
   async def on_ready():
     print(f'{client.user} is now running!')
+
+  @client.event
+  async def on_message(message):
+    if message.author == client.user:
+      return
+
+    username = str(message.author)
+    user_message = str(message.content)
+    channel = str(message.channel)
+
+    print(f"{username} said: '{user_message}' ({channel})")
+
+    if user_message[0] == '?':
+      user_message = user_message[1:]
+      await send_message=(message, user_message, is_private=True)
+    else:
+      await send_message(message, user_message, is_private=False)
+
+  client.run(TOKEN)
