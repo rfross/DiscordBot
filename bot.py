8bf0ebf5bad05ab2de80cb67a10dad69de0780e9
rfross <48492222+rfross@users.noreply.github.com>
2024-05-29 15:33:56 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
-# TBD
+import discord
+import responses
+
+async def send_message(message, user_message, is_private):
+  try:
+    response = responses.handle_response(user_message)
+    await message.author.send(response) if is_private else await message.channel.send(response)
+  except Exception as e:
+    print(e)
+
+
+def run_discord_bot():
+  TOKEN = 'OTMzNDMyMjMxMTQwMTUxNDU2.G--DFt.1AWphZMoaafs9lIW8UutjvayryZSDmErtcXEBQ'
+  client == discord.Client()
+
+  @client.event
+  async def on_ready():
+    print(f'{client.user} is now running!')
